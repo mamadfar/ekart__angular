71d3f5e549d70be6421a1f396976f64f087bb5da
mohammad farhadi <m.sabah.farhadi@gmail.com>
2023-07-14 22:36:13 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ek-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular16-ekart';
+}
